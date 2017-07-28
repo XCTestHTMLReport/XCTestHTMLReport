e5f1c3e657e7d604e2de5830c91161b0eb4456b0
Titouan Van Belle <titouan.vanbelle@gmail.com>
2017-07-28 23:01:43 +0800

    Install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl -o xchtmlreport https://raw.githubusercontent.com/TitouanVanBelle/XCUITestHTMLReport/master/xchtmlreport
+chmod 755 xchtmlreport
+mv xchtmlreport /usr/local/bin/
+printf "\nSuccessully installed XCUITestHTMLReport. Execute xchtmlreport -h for help"
