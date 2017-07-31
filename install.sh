bed0204cc05aa63cca9ded758838214f12e3822f
Titouan Van Belle <titouan.vanbelle@gmail.com>
2017-07-31 16:52:39 +0800

    Added break line

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 curl -o xchtmlreport https://raw.githubusercontent.com/TitouanVanBelle/XCUITestHTMLReport/master/xchtmlreport
 chmod 755 xchtmlreport
 mv xchtmlreport /usr/local/bin/
-printf "\nSuccessully installed XCUITestHTMLReport. Execute xchtmlreport -h for help"
+printf "\nSuccessully installed XCUITestHTMLReport. Execute xchtmlreport -h for help\n"
