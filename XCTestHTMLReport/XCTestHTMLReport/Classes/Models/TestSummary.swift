6b257a0d84b4f2c47dcdbd932085f19eeeb639db
Titouan Van Belle <titouan.vanbelle@fyber.com>
2018-08-29 21:14:21 +0200

    Remove while loop to prevent command from hanging in case there are no tests

--- XCTestHTMLReport/XCTestHTMLReport/Classes/Models/TestSummary.swift
+++ XCTestHTMLReport/XCTestHTMLReport/Classes/Models/TestSummary.swift
@@ -21,31 +21,29 @@ struct TestSummary: HTML
             return .success
         }
 
-        while status == .unknown {
-            status = currentTests.reduce(.unknown, { (accumulator: Status, test: Test) -> Status in
-                if accumulator == .unknown {
-                    return test.status
-                }
+        status = currentTests.reduce(.unknown, { (accumulator: Status, test: Test) -> Status in
+            if accumulator == .unknown {
+                return test.status
+            }
 
-                if test.status == .failure {
-                    return .failure
-                }
+            if test.status == .failure {
+                return .failure
+            }
 
-                if test.status == .success {
-                    return accumulator == .failure ? .failure : .success
-                }
+            if test.status == .success {
+                return accumulator == .failure ? .failure : .success
+            }
 
-                return .unknown
-            })
+            return .unknown
+        })
 
-            currentTests = currentTests.reduce([], { (accumulator: [Test], test: Test) -> [Test] in
-                if let subTests = test.subTests {
-                    return accumulator + subTests
-                }
+        currentTests = currentTests.reduce([], { (accumulator: [Test], test: Test) -> [Test] in
+            if let subTests = test.subTests {
+                return accumulator + subTests
+            }
 
-                return accumulator
-            })
-        }
+            return accumulator
+        })
 
         return status
     }
