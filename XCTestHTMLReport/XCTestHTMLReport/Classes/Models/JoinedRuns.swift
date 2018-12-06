aece304242fc623b4ac0fe79e501ac99304e8ae1
Alistair Leszkiewicz <aleszkiewicz@grubhub.com>
2018-12-06 04:58:41 -0500

    Added prefixUUIDForJoinedRun

--- XCTestHTMLReport/XCTestHTMLReport/Classes/Models/JoinedRuns.swift
+++ XCTestHTMLReport/XCTestHTMLReport/Classes/Models/JoinedRuns.swift
@@ -42,7 +42,7 @@ struct JoinedRuns : HTML
             "N_OF_TESTS": String(numberOfTests),
             "N_OF_PASSED_TESTS": String(numberOfPassedTests),
             "N_OF_FAILED_TESTS": String(numberOfFailedTests),
-            "TEST_SUMMARIES": testSummaries.map { $0.html }.joined()
+            "TEST_SUMMARIES": testSummaries.map { $0.prefixUUIDForJoinedRunWithChildren() }.map { $0.html }.joined()
         ]
     }
 }
@@ -53,5 +53,57 @@ private extension Array where Element == Int {
     func sum() -> Int {
         return reduce(0, +)
     }
+    
+}
+
+/// To keep these UUID's unique within the HTML document
+/// they are all pre-pended with the constant string 'ALL'
+fileprivate protocol JoinedTestSummaryDisplayProtocol {
+    var uuid: String { get set }
+    func prefixUUIDForJoinedRun() -> Self
+    func prefixUUIDForJoinedRunWithChildren() -> Self
+}
+
+fileprivate extension JoinedTestSummaryDisplayProtocol {
+    
+    func prefixUUIDForJoinedRun() -> Self {
+        var copy = self
+        copy.uuid = "ALL-" + copy.uuid
+        return copy
+    }
+    
+    func prefixUUIDForJoinedRunWithChildren() -> Self {
+        return prefixUUIDForJoinedRun()
+    }
+}
+
+extension TestSummary: JoinedTestSummaryDisplayProtocol {
+    
+    func prefixUUIDForJoinedRunWithChildren() -> TestSummary {
+        var copy = self.prefixUUIDForJoinedRun()
+        copy.tests = copy.tests.map { $0.prefixUUIDForJoinedRunWithChildren() }
+        return copy
+    }
+    
+}
+
+extension Test: JoinedTestSummaryDisplayProtocol {
+    
+    func prefixUUIDForJoinedRunWithChildren() -> Test {
+        var copy = self.prefixUUIDForJoinedRun()
+        copy.subTests = copy.subTests?.map { $0.prefixUUIDForJoinedRunWithChildren() }
+        copy.activities = copy.activities?.map { $0.prefixUUIDForJoinedRunWithChildren() }
+        return copy
+    }
+    
+}
+extension Activity: JoinedTestSummaryDisplayProtocol {
+    
+    func prefixUUIDForJoinedRunWithChildren() -> Activity {
+        var copy = self.prefixUUIDForJoinedRun()
+        copy.subActivities = copy.subActivities?.map { $0.prefixUUIDForJoinedRunWithChildren() }
+        return copy
+    }
+
     
 }
