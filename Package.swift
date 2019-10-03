e9d3d64f7c83d0649767daf8327822d6e51ef8e8
Pierre Felgines <pierre.felgines@fabernovel.com>
2019-10-03 15:54:55 +0200

    Remove useless dependency

--- Package.swift
+++ Package.swift
@@ -7,7 +7,6 @@ let package = Package(
     name: "XCTestHTMLReport",
     dependencies: [
          .package(url: "https://github.com/onevcat/Rainbow.git", from: "3.0.0"),
-         .package(url: "https://github.com/1024jp/GzipSwift.git", from: "5.0.0"),
          .package(url: "https://github.com/davidahouse/XCResultKit.git", .revision("8434203a0b11ebc771a70add09f9f56f1f5e8768"))
     ],
     targets: [
@@ -15,7 +14,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "XCTestHTMLReport",
-            dependencies: ["Rainbow", "Gzip", "XCResultKit"]),
+            dependencies: ["Rainbow", "XCResultKit"]),
         .testTarget(
             name: "XCTestHTMLReportTests",
             dependencies: ["XCTestHTMLReport"]),
--- Sources/XCTestHTMLReport/Classes/Models/Run.swift
+++ Sources/XCTestHTMLReport/Classes/Models/Run.swift
@@ -7,7 +7,6 @@
 //
 
 import Foundation
-import Gzip
 import XCResultKit
 
 struct Run: HTML
