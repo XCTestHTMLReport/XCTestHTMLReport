06701050e8ee959e88fcbce6a4edd881b35206da
Alexander Chapliuk <alexander_chapliuk@epam.com>
2020-06-19 18:37:06 +0200

    Fix brew ignoring `head` when `url` is defined in formulae

--- xchtmlreport.rb
+++ xchtmlreport.rb
@@ -1,7 +1,6 @@
 class Xchtmlreport < Formula
   desc "XCTestHTMLReport: Xcode-like HTML report for Unit and UI Tests"
   homepage "https://github.com/TitouanVanBelle/XCTestHTMLReport"
-  url "https://github.com/TitouanVanBelle/XCTestHTMLReport/archive/2.0.0.tar.gz"
   sha256 "4424d673d578e84e67fd96afa53a5bd3e80ec7acade65365a123af358c77b47e"
   head "https://github.com/TitouanVanBelle/XCTestHTMLReport.git", :branch => "develop"
 
