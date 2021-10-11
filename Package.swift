95cf453a145b321f4b4b37aa07b846b40d66e079
tylervick <tylerjvick@gmail.com>
2021-10-10 19:46:45 -0600

    command parser

--- Package.swift
+++ Package.swift
@@ -1,12 +1,12 @@
-// swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.4
 
 import PackageDescription
 
 let package = Package(
     name: "XCTestHTMLReport",
+    defaultLocalization: "en",
     platforms: [
-        .macOS(.v10_15),
+        .macOS(.v10_15)
     ],
     products: [
         .executable(name: "xchtmlreport", targets: ["XCTestHTMLReport"]),
@@ -14,17 +14,26 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/onevcat/Rainbow.git", .upToNextMajor(from: "3.0.0")),
-        .package(url: "https://github.com/davidahouse/XCResultKit.git", .upToNextMinor(from: "0.9.2")),
+        .package(
+            url: "https://github.com/davidahouse/XCResultKit.git", .upToNextMinor(from: "0.9.2")),
         .package(url: "https://github.com/nacho4d/NDHpple.git", .upToNextMajor(from: "2.0.1")),
+        .package(
+            url: "https://github.com/apple/swift-argument-parser.git", .upToNextMajor(from: "1.0.0")
+        ),
     ],
     targets: [
-        .target(
+        .executableTarget(
             name: "XCTestHTMLReport",
-            dependencies: ["XCTestHTMLReportCore"]),
+            dependencies: [
+                "XCTestHTMLReportCore",
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ],
+            resources: [.process("Resources")]
+        ),
         .target(
             name: "XCTestHTMLReportCore",
             dependencies: ["Rainbow", "XCResultKit"],
-            exclude: ["HTML"]), // ignore HTML directory resources. They are already imported as static strings.
+            exclude: ["HTML"]),  // ignore HTML directory resources. They are already imported as static strings.
         .testTarget(
             name: "XCTestHTMLReportTests",
             dependencies: ["XCTestHTMLReport", "NDHpple"],
