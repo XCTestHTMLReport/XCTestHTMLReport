b5e85a87e2bbee72a85800858566f6a0109852dd
Guillermo Ignacio Enriquez Gutierrez <nacho4d@gmail.com>
2021-10-14 00:20:11 +0900

    update shasum

--- xchtmlreport.rb
+++ xchtmlreport.rb
@@ -2,7 +2,7 @@ class Xchtmlreport < Formula
   desc "XCTestHTMLReport: Xcode-like HTML report for Unit and UI Tests"
   homepage "https://github.com/XCTestHTMLReport/XCTestHTMLReport"
   url "https://github.com/XCTestHTMLReport/XCTestHTMLReport/archive/refs/tags/2.2.0.tar.gz"
-  sha256 "bdab5934a92df2dfbeea40c3f0fefba816b48cb1c125785e87514ec0a534fada"
+  sha256 "bc422443945255ed03a0af6e3f72ed342dd0d1b74dd0d641e6dd773aa6195823"
   license "MIT"
   head "https://github.com/XCTestHTMLReport/XCTestHTMLReport.git", :branch => "main"
 
