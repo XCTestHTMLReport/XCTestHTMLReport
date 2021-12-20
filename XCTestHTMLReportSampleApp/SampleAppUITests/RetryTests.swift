9cb4c71a3cb398d9dc749c5717d667978a0d9245
tylervick <tylerjvick@gmail.com>
2021-12-20 15:33:30 -0700

    dummy

---
+++ XCTestHTMLReportSampleApp/SampleAppUITests/RetryTests.swift
@@ -0,0 +1,39 @@
+//
+//  FourthSuite.swift
+//  
+//
+//  Created by Tyler Vick on 12/20/21.
+//
+
+import XCTest
+import Foundation
+
+class RetryTests: XCTestCase {
+
+    private var filePath: URL {
+        let sharedDir = NSTemporaryDirectory()
+        let persistentFileName = "will_succeed"
+        return URL(fileURLWithPath: sharedDir).appendingPathComponent(persistentFileName)
+    }
+
+    private func writeFile() throws {
+        FileManager.default.createFile(atPath: filePath.path, contents: Data())
+    }
+
+    private func shouldSucceed() -> Bool {
+        FileManager.default.fileExists(atPath: filePath.path)
+    }
+
+    // First iteration will always fail, retry will succeed
+    func testRetryOnFailure() throws {
+        try XCTContext.runActivity(named: "Retryable Activity") { _ in
+            if shouldSucceed() {
+                try FileManager.default.removeItem(at: filePath)
+            } else {
+                try writeFile()
+                XCTFail()
+            }
+        }
+    }
+    
+}
