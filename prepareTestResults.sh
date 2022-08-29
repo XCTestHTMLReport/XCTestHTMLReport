7f880c39bd2d9a74a2d70e6fb8a73a5033b75b29
Tyler Vick <tyler@tylervick.com>
2022-08-29 17:10:07 -0600

    more corrected path

--- prepareTestResults.sh
+++ prepareTestResults.sh
@@ -18,20 +18,20 @@ echo "Even if some test failed this is OK."
 echo "${FILENAME} should contain succeed, failed and skipped tests for xchtmlreport functional testing"
 mkdir -p "../Tests/XCTestHTMLReportTests/Resources/"
 rm -rf "../Tests/XCTestHTMLReportTests/Resources/${FILENAME}"
-mv "$FILENAME" "../Tests/XCTestHTMLReportTests/Resources/${FILENAME}"
+mv "$FILENAME" "../Tests/XCTestHTMLReportTests/Resources/"
 
-FILENAME='SanityResults.xcresult'
-rm -rf "$FILENAME"
+SANITY_FILENAME='SanityResults.xcresult'
+rm -rf "$SANITY_FILENAME"
 xcodebuild test \
     -project SampleApp.xcodeproj \
     -scheme MainScheme \
     -destination 'platform=iOS Simulator,name=iPhone 8,OS=latest' \
     -only-testing:SampleAppUITests/FirstSuite/testOne \
-    -resultBundlePath "$FILENAME" || true
+    -resultBundlePath "$SANITY_FILENAME" || true
 
-echo "${FILENAME} shoudl contain sample data for sanity tests"
-rm -rf "../Tests/XCTestHTMLReportTests/${FILENAME}"
-mv "$FILENAME" "../Tests/XCTestHTMLReportTests/Resources/${FILENAME}"
+echo "${SANITY_FILENAME} should contain sample data for sanity tests"
+rm -rf "../Tests/XCTestHTMLReportTests/${SANITY_FILENAME}"
+mv "$SANITY_FILENAME" "../Tests/XCTestHTMLReportTests/Resources/"
 
 if [[ $XCODE_VERSION != 12.* && $XCODE_VERSION != 11.* ]]; then
     # "Mixed" test results must be run separately to use -retry-tests-on-failure
@@ -47,8 +47,8 @@ if [[ $XCODE_VERSION != 12.* && $XCODE_VERSION != 11.* ]]; then
         -resultBundlePath "$RETRY_FILENAME" || true
 
     echo "${RETRY_FILENAME} will contain mixed test results"
-    rm -rf "../Tests/XCTestHTMLReportTests/${RETRY_FILENAME}"
-    mv "$FILENAME" "../Tests/XCTestHTMLReportTests/Resources/${FILENAME}"
+    rm -rf "../Tests/XCTestHTMLReportTests/Resources/${RETRY_FILENAME}"
+    mv "$RETRY_FILENAME" "../Tests/XCTestHTMLReportTests/Resources/"
 fi
 
 echo "$(tput setaf 2)$(basename "$0") successfully finished$(tput sgr 0)"
