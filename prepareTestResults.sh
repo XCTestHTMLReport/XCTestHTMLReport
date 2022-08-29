916d854bf57a320a1ab8d2911f39e57f9e948f95
Tyler Vick <tyler@tylervick.com>
2022-08-29 16:32:16 -0600

    update resources path

--- prepareTestResults.sh
+++ prepareTestResults.sh
@@ -16,8 +16,9 @@ xcodebuild test \
 echo "Even if some test failed this is OK."
 
 echo "${FILENAME} should contain succeed, failed and skipped tests for xchtmlreport functional testing"
-rm -rf "../Tests/XCTestHTMLReportTests/${FILENAME}"
-mv "$FILENAME" "../Tests/XCTestHTMLReportTests/Resources/"
+mkdir -p "../Tests/XCTestHTMLReportTests/Resources/"
+rm -rf "../Tests/XCTestHTMLReportTests/Resources/${FILENAME}"
+mv "$FILENAME" "../Tests/XCTestHTMLReportTests/Resources/${FILENAME}"
 
 FILENAME='SanityResults.xcresult'
 rm -rf "$FILENAME"
@@ -30,7 +31,7 @@ xcodebuild test \
 
 echo "${FILENAME} shoudl contain sample data for sanity tests"
 rm -rf "../Tests/XCTestHTMLReportTests/${FILENAME}"
-mv "$FILENAME" "../Tests/XCTestHTMLReportTests/Resources/"
+mv "$FILENAME" "../Tests/XCTestHTMLReportTests/Resources/${FILENAME}"
 
 if [[ $XCODE_VERSION != 12.* && $XCODE_VERSION != 11.* ]]; then
     # "Mixed" test results must be run separately to use -retry-tests-on-failure
@@ -47,7 +48,7 @@ if [[ $XCODE_VERSION != 12.* && $XCODE_VERSION != 11.* ]]; then
 
     echo "${RETRY_FILENAME} will contain mixed test results"
     rm -rf "../Tests/XCTestHTMLReportTests/${RETRY_FILENAME}"
-    mv "$RETRY_FILENAME" "../Tests/XCTestHTMLReportTests/Resources/"
+    mv "$FILENAME" "../Tests/XCTestHTMLReportTests/Resources/${FILENAME}"
 fi
 
 echo "$(tput setaf 2)$(basename "$0") successfully finished$(tput sgr 0)"
