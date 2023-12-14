a7ac0e8af51ac194a3557f8ee422af832cd57cc8
Tyler Vick <tyler@tylervick.com>
2023-12-13 17:21:37 -0700

    bump xcresultkit to use xcresulttool version 22007

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .package(url: "https://github.com/onevcat/Rainbow.git", .upToNextMajor(from: "3.0.0")),
         .package(
             url: "https://github.com/tylervick/XCResultKit.git",
-            revision: "d5462510fc09055594b3cf472e443bee0d130043"
+            revision: "3555389dd5cff613945da9bf77dde0bf9754737d"
         ),
         .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.4.3"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
