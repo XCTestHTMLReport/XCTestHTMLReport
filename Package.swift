7ec45992225bb247d0ec569cc1813bc872bdff5d
Yusuf Özgül <yusuf.ozgul@trendyol.com>
2024-12-26 15:05:28 +0300

    Bump XCResultKit version

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/onevcat/Rainbow.git", .upToNextMajor(from: "3.0.0")),
-        .package(url: "https://github.com/davidahouse/XCResultKit.git", from: "1.2.0"),
+        .package(url: "https://github.com/davidahouse/XCResultKit.git", from: "1.2.1"),
         .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.4.3"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
         .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.50.4"),
